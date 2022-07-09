a15c504d7cf7def6e0b5037b07a31eef36ec6627
Tiago César <55418034+cesartiago@users.noreply.github.com>
2022-07-09 13:21:46 -0300

    Update Media.java

--- LP3/Lista1/Exerc6/Media.java
+++ LP3/Lista1/Exerc6/Media.java
@@ -4,13 +4,13 @@ public class Media {
     double Av1;
     double Av2;
     
-    public Media (){
+    public Media (){ //Construtor padrão, vai dar os valores padrão nesse caso, zero (0.0)
            }    
-    public Media (double p1, double p2) {
+    public Media (double p1, double p2) { //Não pode ter return pois é um construtor. E não pode nem escrever void aqui!
     this.Av1 = p1;
     this.Av2 = p2;
     }
-    public double calcularMedia (){
+    public double calcularMedia (){ //Isso pe um MÉTODO, e tem return não vazio
         return (Av1+Av2)/2;
     }
 }
